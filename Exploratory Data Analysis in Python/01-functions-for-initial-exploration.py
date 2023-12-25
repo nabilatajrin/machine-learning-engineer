af756f646d9afeec8a1656635b77fd8c58171023
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-25 16:05:48 +0600

    Create 01-functions-for-initial-exploration.py

---
+++ Exploratory Data Analysis in Python/01-functions-for-initial-exploration.py
@@ -0,0 +1,8 @@
+# Print the first five rows of unemployment
+print(unemployment.head(5))
+
+# Print a summary of non-missing values and data types in the unemployment DataFrame
+print(unemployment.info())
+
+# Print summary statistics for numerical columns in unemployment
+print(unemployment.describe())
