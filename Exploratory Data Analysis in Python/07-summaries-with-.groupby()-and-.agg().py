44c1e72f2ddadf38fa5c9ff2f315c09557f040e3
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:46:53 +0600

    Delete Exploratory Data Analysis in Python/07-summaries-with-.groupby()-and-.agg().py

--- Exploratory Data Analysis in Python/07-summaries-with-.groupby()-and-.agg().py
+++
@@ -1,5 +0,0 @@
-# Print the mean and standard deviation of rates by year
-print(unemployment.agg(['mean', 'std']))
-
-# Print yearly mean and standard deviation grouped by continent
-print(unemployment.groupby('continent').agg(['mean', 'std']))
