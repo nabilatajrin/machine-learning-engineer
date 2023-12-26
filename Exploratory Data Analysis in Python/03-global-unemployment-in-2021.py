18529cc60b1e6ce8285bab2e89caffb911760a8e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:43:21 +0600

    Delete Exploratory Data Analysis in Python/03-global-unemployment-in-2021.py

--- Exploratory Data Analysis in Python/03-global-unemployment-in-2021.py
+++
@@ -1,7 +0,0 @@
-# Import the required visualization libraries
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Create a histogram of 2021 unemployment; show a full percent in each bin
-sns.histplot(data=unemployment, x='2021', binwidth=1)
-plt.show()
