245d15178700a5bf36a516a07a304fe8056c441d
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:45:17 +0600

    Delete Exploratory Data Analysis in Python/04-update-data-types-in-column.py

--- Exploratory Data Analysis in Python/04-update-data-types-in-column.py
+++
@@ -1,4 +0,0 @@
-# Update the data type of the 2019 column to a float
-unemployment["2019"] = unemployment["2019"].astype(float)
-# Print the dtypes to check your work
-print(unemployment.describe())
