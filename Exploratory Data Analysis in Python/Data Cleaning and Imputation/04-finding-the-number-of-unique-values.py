fc9775dd7cc7e141fdbfdebbe626e4463b2e3fd4
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 19:41:09 +0600

    Create 04-finding-the-number-of-unique-values.py

---
+++ Exploratory Data Analysis in Python/Data Cleaning and Imputation/04-finding-the-number-of-unique-values.py
@@ -0,0 +1,8 @@
+# Filter the DataFrame for object columns
+non_numeric = planes.select_dtypes("object")
+
+# Loop through columns
+for col in non_numeric.columns:
+  
+  # Print the number of unique values
+  print(f"Number of unique values in {col} column: ", non_numeric[col].unique())
