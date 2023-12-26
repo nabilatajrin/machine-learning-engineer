aa80af10e226e8e0ffa544f39a43c67157e60347
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 17:38:50 +0600

    Create 01-dealing-with-missing-data.py

---
+++ Exploratory Data Analysis in Python/Data Cleaning and Imputation/01-dealing-with-missing-data.py
@@ -0,0 +1,13 @@
+# Count the number of missing values in each column
+print(planes.isna().sum())
+
+# Find the five percent threshold
+threshold = len(planes) * 0.05
+
+# Create a filter
+cols_to_drop = planes.columns[planes.isna().sum() <= threshold]
+
+# Drop missing values for columns below the threshold
+planes.dropna(subset=cols_to_drop, inplace=True)
+
+print(planes.isna().sum())
