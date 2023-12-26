f717eba93b31512b2e052a8fa94a889bda57cdb4
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:42:00 +0600

    Delete Exploratory Data Analysis in Python/01-functions-for-initial-exploration.py

--- Exploratory Data Analysis in Python/01-functions-for-initial-exploration.py
+++
@@ -1,8 +0,0 @@
-# Print the first five rows of unemployment
-print(unemployment.head(5))
-
-# Print a summary of non-missing values and data types in the unemployment DataFrame
-print(unemployment.info())
-
-# Print summary statistics for numerical columns in unemployment
-print(unemployment.describe())
