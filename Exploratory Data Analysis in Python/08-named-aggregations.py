f1a682f0dd64841541a025325ae10dc26985acaa
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:47:28 +0600

    Delete Exploratory Data Analysis in Python/08-named-aggregations.py

--- Exploratory Data Analysis in Python/08-named-aggregations.py
+++
@@ -1,7 +0,0 @@
-continent_summary = unemployment.groupby("continent").agg(
-    # Create the mean_rate_2021 column
-    mean_rate_2021 = ('2021','mean'),
-    # Create the std_rate_2021 column
-    std_rate_2021 = ('2021','std'),
-)
-print(continent_summary)
