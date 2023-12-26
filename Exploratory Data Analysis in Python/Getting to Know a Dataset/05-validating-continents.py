a5bd5cb03e6da1c75bc5c6ed9e3c20393f6e8787
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-26 08:45:40 +0600

    Create 05-validating-continents.py

---
+++ Exploratory Data Analysis in Python/Getting to Know a Dataset/05-validating-continents.py
@@ -0,0 +1,5 @@
+# Define a Series describing whether each continent is outside of Oceania
+not_oceania = ~unemployment["continent"].isin(["Oceania"])
+
+# Print unemployment without records related to countries in Oceania
+print(unemployment[not_oceania])
