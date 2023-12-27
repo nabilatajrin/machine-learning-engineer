4fd2fb06e53c66b359097246dedc2283969b684f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-27 12:43:52 +0600

    Create 06-adding-duration-categories.py

---
+++ Exploratory Data Analysis in Python/Data Cleaning and Imputation/06-adding-duration-categories.py
@@ -0,0 +1,15 @@
+# Create conditions for values in flight_categories to be created
+conditions = [
+    (planes["Duration"].str.contains(short_flights)),
+    (planes["Duration"].str.contains(medium_flights)),
+    (planes["Duration"].str.contains(long_flights))
+]
+
+# Apply the conditions list to the flight_categories
+planes["Duration_Category"] = np.select(conditions, 
+                                        flight_categories,
+                                        default="Extreme duration")
+
+# Plot the counts of each category
+sns.countplot(data=planes, x="Duration_Category")
+plt.show()
