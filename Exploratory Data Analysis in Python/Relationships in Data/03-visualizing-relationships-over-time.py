15271321f11ca45955b78e063de6f03d03998a10
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-28 19:26:03 +0600

    Update 03-visualizing-relationships-over-time.py

--- Exploratory Data Analysis in Python/Relationships in Data/03-visualizing-relationships-over-time.py
+++ Exploratory Data Analysis in Python/Relationships in Data/03-visualizing-relationships-over-time.py
@@ -1,2 +1,6 @@
 # Define the marriage_year column
-divorce["marriage_year"] = divorce['marriage_date'].dt.year
+divorce["marriage_year"] = divorce["marriage_date"].dt.year
+
+# Create a line plot showing the average number of kids by year
+sns.lineplot(data=divorce, x="marriage_year", y='num_kids')
+plt.show()
