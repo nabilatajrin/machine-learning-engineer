b0809b91fb59a56564891d13b0eed9c5ab4cff18
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-29 14:16:47 +0600

    Create 06-comparing-salaries.py

---
+++ Exploratory Data Analysis in Python/Turning Exploratory Analysis into Action/06-comparing-salaries.py
@@ -0,0 +1,6 @@
+# Filter for employees in the US or GB
+usa_and_gb = salaries[salaries["Employee_Location"].isin(["US", "GB"])]
+
+# Create a barplot of salaries by location
+sns.barplot(data=usa_and_gb, x="Employee_Location", y="Salary_USD")
+plt.show()
