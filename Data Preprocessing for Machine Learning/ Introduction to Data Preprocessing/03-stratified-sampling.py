76c10b0921ba06939087143d19e867fce6f1800e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-30 18:30:29 +0600

    Create 03-stratified-sampling.py

---
+++ Data Preprocessing for Machine Learning/ Introduction to Data Preprocessing/03-stratified-sampling.py
@@ -0,0 +1,11 @@
+# Create a DataFrame with all columns except category_desc
+X = volunteer.drop('category_desc', axis=1)
+
+# Create a category_desc labels dataset
+y = volunteer[['category_desc']]
+
+# Use stratified sampling to split up the dataset according to the y dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+# Print the category_desc counts from y_train
+print(y_train['category_desc'].value_counts())
