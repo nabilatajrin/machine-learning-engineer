974ae13f6794da1a85c21d50ad67cd927344b04f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-04 11:44:55 +0600

    Create 05-KNN-on-scaled-data.py

---
+++ Data Preprocessing for Machine Learning/Standardizing Data/05-KNN-on-scaled-data.py
@@ -0,0 +1,14 @@
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+# Instantiate a StandardScaler
+scaler = StandardScaler()
+
+# Scale the training and test features
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train_scaled, y_train)
+
+# Score the model on the test data
+print(knn.score(X_test_scaled, y_test))
