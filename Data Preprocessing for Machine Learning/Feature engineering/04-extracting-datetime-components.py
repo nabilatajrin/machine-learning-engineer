d69784e2774b4c3b6d4bfa8ca5eefcb077e2b0ef
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-05 11:34:53 +0600

    Create 04-extracting-datetime-components.py

---
+++ Data Preprocessing for Machine Learning/Feature engineering/04-extracting-datetime-components.py
@@ -0,0 +1,8 @@
+# First, convert string column to date column
+volunteer["start_date_converted"] = pd.to_datetime(volunteer["start_date_date"])
+
+# Extract just the month from the converted column
+volunteer["start_date_month"] = volunteer["start_date_converted"].dt.month
+
+# Take a look at the converted and new month columns
+print(volunteer[["start_date_converted", "start_date_month"]].head())
