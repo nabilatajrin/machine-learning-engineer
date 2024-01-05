83c567d19c88e8504593d96b1ff0d4b65fda6d13
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-05 11:02:52 +0600

    Create 01-encoding-categorical-variables-binary.py

---
+++ Data Preprocessing for Machine Learning/Feature engineering/01-encoding-categorical-variables-binary.py
@@ -0,0 +1,8 @@
+# Set up the LabelEncoder object
+enc = LabelEncoder()
+
+# Apply the encoding to the "Accessible" column
+hiking["Accessible_enc"] = enc.fit_transform(hiking["Accessible"])
+
+# Compare the two columns
+print(hiking[["Accessible_enc", "Accessible"]].head())
