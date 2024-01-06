e05fc121d3a63adb78869b78e0e1ee9df5f6a7a2
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-06 16:14:51 +0600

    Create 06-vectorizing-text.py

---
+++ Data Preprocessing for Machine Learning/Feature engineering/06-vectorizing-text.py
@@ -0,0 +1,8 @@
+# Take the title text
+title_text = volunteer["title"]
+
+# Create the vectorizer method
+tfidf_vec = TfidfVectorizer()
+
+# Transform the text into tf-idf vectors
+text_tfidf = tfidf_vec.fit_transform(title_text)
