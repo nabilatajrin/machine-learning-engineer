cb16f5a3dcd66518bbb0ac30bbc78b452c56388a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-06 16:21:19 +0600

    Create 07-text-classification-using tf-idf-vectors.py

---
+++ Data Preprocessing for Machine Learning/Feature engineering/07-text-classification-using tf-idf-vectors.py
@@ -0,0 +1,9 @@
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+X_train, X_test, y_train, y_test = train_test_split(text_tfidf.toarray(), y, stratify=y, random_state=42)
+
+# Fit the model to the training data
+nb.fit(X_train, y_train)
+
+# Print out the model's accuracy
+print(nb.score(X_test, y_test))
