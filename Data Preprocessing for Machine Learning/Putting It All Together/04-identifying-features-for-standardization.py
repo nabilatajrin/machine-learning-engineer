368cae8e172588aa931cfb3669587e226dc2254e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-12 13:07:10 +0600

    Update and rename 04- to 04-identifying-features-for-standardization.py

--- Data Preprocessing for Machine Learning/Putting It All Together/04-identifying-features-for-standardization.py
+++ Data Preprocessing for Machine Learning/Putting It All Together/04-identifying-features-for-standardization.py
@@ -1,8 +1,8 @@
 # Check the variance of the seconds and minutes columns
-print(ufo['seconds'])
+print(ufo[["seconds", "minutes"]].var())
 
 # Log normalize the seconds column
 ufo["seconds_log"] = np.log(ufo["seconds"])
 
 # Print out the variance of just the seconds_log column
-print(ufo["seconds_log"])
+print(ufo["seconds_log"].var())
