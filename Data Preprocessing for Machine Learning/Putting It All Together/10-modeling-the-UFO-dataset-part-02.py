40c473fb75c273221e33057c0cea195a308a37d0
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-01-13 12:08:55 +0600

    Update 10-modeling-the-UFO-dataset-part-02.py

--- Data Preprocessing for Machine Learning/Putting It All Together/10-modeling-the-UFO-dataset-part-02.py
+++ Data Preprocessing for Machine Learning/Putting It All Together/10-modeling-the-UFO-dataset-part-02.py
@@ -2,10 +2,10 @@
 filtered_text = desc_tfidf[:, list(filtered_words)]
 
 # Split the X and y sets using train_test_split, setting stratify=y 
-X_train, X_test, y_train, y_test = train_test_split(filtered_text.toarray(), y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(filtered_text.toarray(), y, stratify=y, random_state=42)
 
 # Fit nb to the training sets
 nb.fit(X_train, y_train)
 
 # Print the score of nb on the test sets
-print(nb.score(X_train, y_train))
+print(nb.score(X_test, y_test))
