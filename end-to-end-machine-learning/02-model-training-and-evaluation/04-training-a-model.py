c875c7a0a417f44a7d9a3179d9a98fc47830049d
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-02-27 20:14:09 +0600

    Create 04-training-a-model.py

---
+++ end-to-end-machine-learning/02-model-training-and-evaluation/04-training-a-model.py
@@ -0,0 +1,14 @@
+# Import required modules
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+# Split the data into training and testing sets (80:20)
+X_train, X_test, y_train, y_test = train_test_split(heart_disease_X, heart_disease_y, test_size=0.2, random_state=42)
+
+# Define the SVM / SVC model
+svc_model = SVC(kernel='linear')
+svc_model.fit(X_train, y_train)
+
+# Get predictions from the model
+y_pred = svc_model.predict(X_test)
+print(y_pred)
