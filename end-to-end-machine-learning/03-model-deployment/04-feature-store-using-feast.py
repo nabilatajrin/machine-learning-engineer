49ae6890ca1128d8da0d198f73c4243215bc5133
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-02-28 14:55:25 +0600

    Create 04-feature-store-using-feast.py

---
+++ end-to-end-machine-learning/03-model-deployment/04-feature-store-using-feast.py
@@ -0,0 +1,20 @@
+heart_disease_df.to_parquet("heart_disease.parquet")
+
+# Point File Source to the saved file
+data_source = FileSource(
+    path="heart_disease.parquet",
+    event_timestamp_column="timestamp",
+    created_timestamp_column="created",
+)
+
+# Create a Feature View of the features
+heart_disease_fv = FeatureView(
+    name="heart_disease",
+    entities=[patient],
+    schema=[cp, thalach, ca, thal],
+    source=data_source,
+)
+
+# Create a store of the data and apply the features
+store = FeatureStore(repo_path=".")
+store.apply([patient, heart_disease_fv])
